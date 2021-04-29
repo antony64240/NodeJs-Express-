43d058e11b5a15835c8337522893da29584872dc
Adam MEZZASALMA <45571593+antony64240@users.noreply.github.com>
2021-04-29 12:33:14 +0200

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -9,27 +9,27 @@ const { checkToken , verifyToken } = require("../controlleurs/TokenValidation");
 
 
 //ANONYMOUS ROUTE
-router.post('/AddUser', SignUser.createUsers);
-router.post('/LoginUser', AuthUser.authentification);
-router.get('/verifyEmail/:token', SignUser.verifyEmail);
-router.post('/ForgotPassword', AuthUser.recoverPassword);
-router.get('/ForgotPassword', AuthUser.forgotPassword);
+router.post('/', SignUser.createUsers);
+router.post('/', AuthUser.authentification);
+router.get('//:token', SignUser.verifyEmail);
+router.post('/', AuthUser.recoverPassword);
+router.get('/', AuthUser.forgotPassword);
 
 
 //ADMINROUTE
-router.get('/Users', SignUser.getUsers);
-router.delete('/DeletUser/:id', SignUser.DeletOneUser);
-router.post('/createpdf', Project.createDevis)
+router.get('/', SignUser.getUsers);
+router.delete('//:id', SignUser.DeletOneUser);
+router.post('/', Project.createDevis)
 
 
 //USERROUTE
-router.get('/Download', checkToken, FileUser.downloadFiles)
-router.get('/ListFichier',checkToken, FileUser.filesList);
-router.get('/CheckToken',verifyToken);
-router.post('/UploadFile', FileUser.UploadFile);
-router.post('/UpdateUser',checkToken, AuthUser.updateUser);
-router.post('/newProject',checkToken, Project.createProject);
-router.get('/getProject/:Email',checkToken, Project.getProjectbyUsers);
-router.get('/Project/:token', Project.getProject );
+router.get('/', checkToken, FileUser.downloadFiles)
+router.get('/',checkToken, FileUser.filesList);
+router.get('/',verifyToken);
+router.post('/', FileUser.UploadFile);
+router.post('/',checkToken, AuthUser.updateUser);
+router.post('/',checkToken, Project.createProject);
+router.get('//:Email',checkToken, Project.getProjectbyUsers);
+router.get('//:token', Project.getProject );
 
 module.exports = router;
